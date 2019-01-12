5836105c8f13661e72bf71b2bd01be0bfc7c9969
e <wlx100715@163.com>
2019-01-12 20:59:28 +0800

    Re-understanding the requirements and modify the code, add some styles.

--- js/service.js
+++ js/service.js
@@ -1,8 +1,27 @@
 function loadTodos() {
   const todos = getFromStorage();
+  document.getElementById('all').style.background = 'white';
   if (todos != -1) {
     showByState(todos, 'all');
   }
+}
+function getTodo() {
+  const todoString = document.getElementById('add').value;
+  if (!todoString.length) {
+    return -1;
+  }
+  return new Todo(new Date().getMilliseconds(), todoString, 'active');  
+}
+function checkEnterAddTodo() {
+  const todo = getTodo();
+  if (checkEnter()) {
+    saveToStorage(todo);
+    updateTodos();
+  }
+}
+function checkEnter() {
+  const todoString = document.getElementById('add').value;
+  return event.keyCode == 13 && todoString.length;
 }
 function getState() {
   const activeColor = document.getElementById('active').style.background;
@@ -15,45 +34,6 @@ function getState() {
   }
   return 'all';
 }
-function getTodo(state) {
-  const todoString = document.getElementById('add').value;
-  if (!todoString.length) {
-    return -1;
-  }
-  if (state != 'completed') {
-    state = 'active';
-  }
-  return new Todo(new Date().getMilliseconds(), todoString, state);  
-}
-function addTodo() {
-  const state = getState();
-  const todo = getTodo(state);
-  if (todo != -1) {
-    saveToStorage(todo);
-  }
-}
-function checkEnterAddTodo() {
-  const state = getState();
-  const todo = getTodo(state);
-  if (checkEnter()) {
-    saveToStorage(todo);
-    updateTodosAfterAdd();
-  }
-}
-function checkEnter() {
-  const todoString = document.getElementById('add').value;
-  return event.keyCode == 13 && todoString.length;
-}
-function updateTodosAfterAdd() {
-  const state = getState();
-  const todos = getFromStorage();
-  if (state === 'completed') {
-    const completedTodos = todos.filter(todo => todo.state === 'completed');
-    updateInputAndItems(completedTodos);
-  } else {
-    showByState(state);
-  }
-}
 function updateTodos() {
   const state = getState();
   showByState(state);
@@ -61,9 +41,9 @@ function updateTodos() {
 function showByState(state) {
   const todos = getFromStorage();
   showClear(todos);
+  updateInputAndItems(todos);
   const selectedTodos = selectTodos(todos, state);
   showTodos(selectedTodos);
-  updateInputAndItems(selectedTodos);
 }
 function selectTodos(todos, state) {
   switch (state) {
@@ -86,27 +66,28 @@ function showClear(todos) {
 }
 function showTodos(todos) {
   if (todos.length) {
-    let todosLabel = generateLabel(todos);
-    document.getElementById('list').innerHTML = `<ul>${todosLabel.join('\n')}</ul>`
+    let todosList = generateList(todos);
+    document.getElementById('list').innerHTML = `<ul>${todosList.join('\n')}</ul>`
   } else {
     document.getElementById('list').innerHTML = '';
   }
 }
-function generateLabel(todos) {
+function generateList(todos) {
   return todos.map(todo =>
     `<li class=${todo.state} id=${todo.id} onclick="setCompleted(this)">
-        ${todo.content}
-        <button class="delete" type="button" onclick="deleteTodo(this)">×</button>
+      ${todo.content}
+      <button class="delete" type="button" onclick="deleteTodo(this)">×</button>
     </li>`);
 }
 function updateInputAndItems(todos) {
-  document.getElementById('items').innerHTML = `left items:${todos.length}`;
+  const activeTodos = todos.filter(todo => todo.state === 'active');
+  document.getElementById('items').innerHTML = `left items:${activeTodos.length}`;
   document.getElementById('add').value = '';
 }
 function buttonInit() {
-  document.getElementById('all').style.background = '#e0bb53';
-  document.getElementById('active').style.background = '#e0bb53';
-  document.getElementById('completed').style.background = '#e0bb53';
+  document.getElementById('all').style.background = '#85dfe9';
+  document.getElementById('active').style.background = '#85dfe9';
+  document.getElementById('completed').style.background = '#85dfe9';
 }
 function changeState(ele) {
   const todos = getFromStorage();
